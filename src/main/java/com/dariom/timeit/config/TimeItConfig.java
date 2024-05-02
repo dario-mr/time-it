f1ae00df5e88d14156973565da5fe70077c31e78
Dario Mauri <dario.mauri_ext@zooplus.com>
2024-05-02 10:32:27 +0200

    Add auto-configuration imports
    This tells the consumers of this library which packages to scan for components

---
+++ src/main/java/com/dariom/timeit/config/TimeItConfig.java
@@ -0,0 +1,9 @@
+package com.dariom.timeit.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.dariom.timeit")
+class TimeItConfig {
+}
